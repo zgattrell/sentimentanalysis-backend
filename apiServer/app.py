b80e59da3145e00cfd8e21bb6a304722ec8d6638
zgattrell <zacharygattrell@gmail.com>
2022-02-12 14:37:01 -0600

    api endpoint now takes timeFilter parameter

--- apiServer/app.py
+++ apiServer/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+import time
+CORS(app)
 import redditUtil
 
 @app.route("/getRedditSentimentChart")
@@ -7,11 +10,16 @@ def getSentimentChart():
     #set defaults for arguments
     symbol ='AMZN'
     range ='5y'
+    tf = 'all'
     limit = 60
+    #get arguments
+    tf = request.args.get('time_filter')
     symbol = request.args.get('symbol')
     range = request.args.get('range')
     limit = int(request.args.get('limit'))
-    chartData = redditUtil.getChartData(symbol, range, limit)
+    b4time = time.time()
+    chartData = redditUtil.getChartData(symbol, range, limit, tf)
+    print(time.time()-b4time)
     
 
     return jsonify(chartData)
--- apiServer/redditUtil.py
+++ apiServer/redditUtil.py
@@ -1,8 +1,9 @@
 from tokenize import Comment
 import praw as Praw
 from textblob import TextBlob
+from collections import OrderedDict
 
-def getChartData(symbol, range, limit):
+def getChartData(symbol, range, limit, tf):
     #init variables
     chartData = {}
     labels = []
@@ -13,19 +14,26 @@ def getChartData(symbol, range, limit):
                          user_agent='MemeScraper1.0')
 
     subreddit = reddit.subreddit('stocks')
-    for submission in subreddit.search(symbol, limit=limit):
+    for submission in subreddit.search(symbol,'new', limit=limit, time_filter=tf):
         print('reading message ' + submission.title)
         #add the timestamp of the post to labels list
         labels.append(submission.created_utc)
         #add the sentiment to the datapoints list
-        datapoints.append(getSentiment(submission))
+        postSent = getSentiment(submission)
+        datapoints.append(postSent)
         postData[submission.title] = {'selftext':submission.selftext,
          'upvoteRatio':submission.upvote_ratio,
          'score':submission.score,
          'url': submission.url,
-         'post_sentiment': getPostSentiment(submission),
-         'comment_sentiment': getCommentSentiment(submission)}
-    
+         'created_utc': submission.created_utc,
+         'post_sentiment': postSent, 
+         #find a way to  use value being returned in datapoints her
+         #'comment_sentiment': getCommentSentiment(submission)
+         }
+    #Reverse all lists because of reddit search setting 'new' getting most recent posts first
+    #labels = labels.reverse() - labels not used anyway
+    datapoints.reverse()
+    #postData = postData.reverse()
 
     #Nest data inside chartData object and return.
     chartData['labels'] = labels
@@ -36,8 +44,10 @@ def getChartData(symbol, range, limit):
 
 #returns sentiment data combined with post stats for upvote + upvote ratio
 def getSentiment(redditPost):
-    post_sentiment = getPostSentiment(redditPost) + getCommentSentiment(redditPost)
-    sentiment_score = post_sentiment * (redditPost.score * redditPost.upvote_ratio)
+    #post_sentiment = getPostSentiment(redditPost) + getCommentSentiment(redditPost)
+    post_sentiment = getPostSentiment(redditPost)
+    #sentiment_score = post_sentiment * (redditPost.score * redditPost.upvote_ratio)
+    sentiment_score = post_sentiment
     return sentiment_score
 
 #Returns sentiment data for reddit post
@@ -45,7 +55,8 @@ def getPostSentiment(post):
     title_sentiment = TextBlob(post.title).sentiment
     selftext_sentiment = TextBlob(post.selftext).sentiment
     #mutliply the polarity and subjectivity of both title and selftext and add them together.
-    post_sentiment = ((title_sentiment.polarity * title_sentiment.subjectivity) + (selftext_sentiment.polarity * selftext_sentiment.subjectivity))
+    #EDIT: only using polarity subjectivity is not important for right now.
+    post_sentiment = ((title_sentiment.polarity) + (selftext_sentiment.polarity)) + 0.5
     return post_sentiment
 
 #Returnss sentiment data for reddit comments
